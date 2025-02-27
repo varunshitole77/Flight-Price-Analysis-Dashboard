b9a86ec4161affcb683e5579ad7d403912b8ccb2
Varun Shitole <vshitole77@gmail.com>
2025-02-27 12:44:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.title("Power BI Dashboard in Streamlit")
 # Replace with your Power BI embed URL
 power_bi_url = "https://app.powerbi.com/view?r=eyJrIjoiOTYxN2M2ZmQtNjNhMy00YzhhLTlmNWQtMjNkYzM5ZWY2NjcwIiwidCI6ImE4ZDNlMTJhLWE0ZjktNDdjMy1iN2JkLTBmMThmNTBjNDcxMyIsImMiOjN9"
 
-st.components.v1.iframe(power_bi_url, width=900, height=600)
+st.components.v1.iframe(power_bi_url, width=1600, height=1600)
