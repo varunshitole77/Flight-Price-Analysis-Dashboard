6dde0df747e48be120a381867ac124564c932eca
Varun Shitole <vshitole77@gmail.com>
2025-02-27 12:50:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Centering the title using Markdown and HTML
-st.markdown("<h1 style='text-align: center;'>Power BI Dashboard in Streamlit</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>Flight Price Analysis Dashboard</h1>", unsafe_allow_html=True)
 
 # Replace with your Power BI embed URL
 power_bi_url = "https://app.powerbi.com/view?r=eyJrIjoiOTYxN2M2ZmQtNjNhMy00YzhhLTlmNWQtMjNkYzM5ZWY2NjcwIiwidCI6ImE4ZDNlMTJhLWE0ZjktNDdjMy1iN2JkLTBmMThmNTBjNDcxMyIsImMiOjN9"
